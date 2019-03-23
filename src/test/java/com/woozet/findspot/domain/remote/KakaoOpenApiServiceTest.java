a18bc2a49cb170ca53b9a858dca5487b8624f6b9
Jun Woo <dyoona@gmail.com>
2019-03-23 18:56:31 +0900

    add test condition for openapi

--- src/test/java/com/woozet/findspot/domain/remote/KakaoOpenApiServiceTest.java
+++ src/test/java/com/woozet/findspot/domain/remote/KakaoOpenApiServiceTest.java
@@ -1,6 +1,7 @@
 package com.woozet.findspot.domain.remote;
 
 import com.woozet.findspot.domain.model.dto.KakaoResource;
+import feign.FeignException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,23 @@ public class KakaoOpenApiServiceTest {
     private KaKaoOpenApiService service;
 
     @Test
-    public void findByKeyword() {
+    public void findByKeyword_정상() {
         KakaoResource response = service.findByKeyword("하동관", 1, 10);
 
         assertThat(response).isNotNull();
         assertThat(response.getDocuments().size()).isNotZero();
+    }
+
+    @Test(expected = FeignException.class)
+    public void findByKeyword_빈문자열() {
+        service.findByKeyword("", 1, 10);
+    }
+
+    @Test
+    public void findByKeyword_검색결과없음() {
+        KakaoResource response = service.findByKeyword("괞찮낳엲잃렇겏젃윾멶검샋겳괍앉낧옭닋깏", 1, 10);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getDocuments().size()).isZero();
     }
 }
